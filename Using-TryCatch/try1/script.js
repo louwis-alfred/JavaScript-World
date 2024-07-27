2b02d3de7fc8eb8623c45ff499176220d0f4eb9f
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-07-28 00:36:09 +0800

    Add files via upload

---
+++ Using-TryCatch/try1/script.js
@@ -0,0 +1,15 @@
+// Try Catch helps you to continuously run the code w/ error
+
+
+// Try Catch
+greet.toUpperCase();
+
+// With Try Catch
+try {
+    greet.toUpperCase();
+} catch {
+    console.log('Error')
+}
+
+console.log('After')
+
