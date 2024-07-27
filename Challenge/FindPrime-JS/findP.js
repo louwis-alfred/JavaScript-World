16b672add34b1a522284ca3de9fd615edb3ed40b
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-07-28 02:33:14 +0800

    Add files via upload

---
+++ Challenge/FindPrime-JS/findP.js
@@ -0,0 +1,20 @@
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13]; 
+        
+function findPrime(n) {
+    for (let i = 2; n > i; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return n > 1
+}
+
+console.log(array.filter(findPrime))
+
+
+// ANSWER:  2, 3, 5, 7, 11, 13  
+
+names = ['Bibeng','Kapitan','Macmac']
+for (let i = 0; i < names.length; i++){
+    console.log(`Hello, ${names[i]}`)
+}
