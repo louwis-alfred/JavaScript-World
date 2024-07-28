7e220c32d100f5ec0573c149f2f82726984c0084
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-07-29 00:24:58 +0800

    Add files via upload

---
+++ Challenge/mapChallenge/map.js
@@ -0,0 +1,6 @@
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+
+const firstName = fullNames.map (function(fn) {
+    return fn.first;
+})
