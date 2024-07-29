aef88206a9d373085f642b4ed303bbd7e8ab790f
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-07-29 20:38:35 +0800

    Update findP.js

--- Challenge/FindPrime-JS/findP.js
+++ Challenge/FindPrime-JS/findP.js
@@ -13,3 +13,19 @@ console.log(array.filter(findPrime))
 
 
 // ANSWER:  2, 3, 5, 7, 11, 13  
+
+
+const array2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13];
+
+function findPrime(n) {
+    if (n <= 1) return false;
+    if (n === 2) return true;
+    for (let i = 2; i <= Math.sqrt(n); i ++) {
+        if (n % i === 0){
+            return false;
+        }
+
+    }
+
+    return true;
+}
