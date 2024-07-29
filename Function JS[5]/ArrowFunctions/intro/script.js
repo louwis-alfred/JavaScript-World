6b180fc10be61ee4fc6f89705f098f1b657227f6
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-07-29 22:20:09 +0800

    Add files via upload

---
+++ Function JS[5]/ArrowFunctions/intro/script.js
@@ -0,0 +1,17 @@
+// Function expression example
+// const add = function(x,y) {
+//     return x + y
+// }
+
+// Alternative to arrow function to be effective 
+const add = (x,y) => {
+    return x + y
+}
+// add(4,3);
+// Output: 7
+const square = (x) => {
+    return x * x
+}
+
+// square(4);
+// Output: 16
