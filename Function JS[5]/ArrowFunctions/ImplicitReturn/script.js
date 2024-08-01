cbbcd48b4c47ceac865a1d4a9efe2b55b119e149
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-01 19:42:40 +0800

    Add files via upload

---
+++ Function JS[5]/ArrowFunctions/ImplicitReturn/script.js
@@ -0,0 +1,11 @@
+// You can omit "return" keyword to "()" if the function body is a single expression
+
+// Example # 1
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+// Example # 2
+const add = (x,y) => (
+    x + y
+)
