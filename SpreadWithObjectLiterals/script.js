f245214d24655bbadebb59d16584e4f0495d81e5
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-05 23:29:34 +0800

    Delete SpreadWithObjectLiterals directory

--- SpreadWithObjectLiterals/script.js
+++
@@ -1,5 +0,0 @@
-let dog =  {legs: 4, breed: 'aspin'}
-console.log(`Original: ${JSON.stringify(dog)}`)
-// To add a new property
-let newProperty = {...dog, color: 'black'}
-console.log(`New: ${JSON.stringify(newProperty)}`)
