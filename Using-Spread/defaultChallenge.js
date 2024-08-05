bc1614979c3541547628ab669e94632936f3c069
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-05 23:48:23 +0800

    Add files via upload

---
+++ Using-Spread/defaultChallenge.js
@@ -0,0 +1,5 @@
+const raceResults = (gold, silver, ...everyOneElse) => {
+    console.log(`Gold: ${gold}`)
+    console.log(`Silver: ${silver}`)
+    console.log(`Thanks to everyone who'd join ${everyOneElse}`)
+}
