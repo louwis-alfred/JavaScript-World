e5e32e9fea7859c8ab17b5b0918472037a6f56d0
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-11 10:25:59 +0800

    Add files via upload

---
+++ DOM [1]/MagicalForest/script.js
@@ -0,0 +1,7 @@
+
+const containerDiv = document.getElementById('container');
+containerDiv.style.textAlign = 'center';
+
+const imageElement = document.querySelector('img');
+imageElement.style.width = '150px';
+imageElement.style.borderRadius = '50%';
---
+++ DOM [1]/WhoCameFirst/chickenEgg.js
@@ -0,0 +1,4 @@
+// The url you need: 'https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200'
+document.querySelector('h4').innerText = 'Chicken'
+document.querySelector('img').src = 'https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200'
+document.querySelector('img').alt = 'chicken'
