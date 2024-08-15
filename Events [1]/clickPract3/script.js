d9a946782539b2ee5a0bfc6c1f259c5463ffb3f6
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-16 00:31:54 +0800

    Add files via upload

---
+++ Events [1]/clickPract3/script.js
@@ -0,0 +1,16 @@
+// addEventListener
+// Specify the event type and a callback to run
+
+
+
+// Instead of this 
+const btn1 = document.querySelector('.btn-example1').onclick = () => {
+    alert('This is onClick')
+}
+
+// We're going to use this 
+const btn2 = document.querySelector('.btn-example2');
+
+btn2.addEventListener('click', () => {
+    alert('This is addEventListener')
+})
