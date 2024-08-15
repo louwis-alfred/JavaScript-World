c8d8d852eb64748e43e45f67cd355cedc4a9dddd
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-16 01:58:43 +0800

    Update script.js

--- Events [1]/RandomColor/script.js
+++ Events [1]/RandomColor/script.js
@@ -1,12 +1,17 @@
 const h1 = document.querySelector('h1');
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
-    document.body.style.backgroundColor = 'olive';
-    const r = Math.floor(Math.random() * 255)
-    const g = Math.floor(Math.random() * 255)
-    const b = Math.floor(Math.random() * 255)
-    const newColor = `rgb(${r}, ${g}, ${b})`;
+    const newColor = makeRandomColor();
     document.body.style.backgroundColor = newColor;
     h1.innerHTML = newColor;
 })
 
+
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+    const newColor = `rgb(${r}, ${g}, ${b})`;
+
+    return newColor;
+}
