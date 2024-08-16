07acfcb68341fd5412cd6c89be09140a3fad7345
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-16 19:26:35 +0800

    Update script.js

--- Events [1]/KeyboardEvents/script.js
+++ Events [1]/KeyboardEvents/script.js
@@ -1,6 +1,18 @@
 const input = document.querySelector('input');
+
+// Key down
 input.addEventListener('keydown', (e) => {
     console.log(e.key)
     console.log(e.code)
 })
 
+// i.e A social media platform or a text-based application where users need to be aware of the character limit.
+
+
+// Key up
+input.addEventListener('keyup', () => {
+    console.log('Logging in')
+})
+
+// Perfect example for "keyup" is that, if the user done typing her's input
+// the account will automatically login
