e30a9067154f0616f095802b84646f8f2cffcdb1
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-17 20:16:15 +0800

    Update script.js

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,16 +1,19 @@
-let input = prompt('What would you like to do?: ');
+// Initialize variables
+let input = prompt('What would you like to do?: '); 
 const todos = ['Jogging','Coding'];
+
+// While loop condition
 while (input !== 'q' && input !== 'quit') {
     if (input === 'list') {
         console.log("Here's the list:")
         for (let i = 0; i < todos.length; i ++) {
             console.log(`${i}: ${todos[i]}`)
         }
-    } else if (input === 'new') {
+    } else if (input === 'new') { // Add a new Todo
         const newTodo = prompt('Ok, What is the new to do?');
         todos.push(newTodo);
         console.log(`${newTodo} is added to the list`)
-    } else if (input === 'delete') {
+    } else if (input === 'delete') { // Delete a todo
         const indextoDelete = parseInt(prompt('Enter an index to delete: '));
         if (!Number.isNaN(indextoDelete)) {
             const deletedIndex = todos.splice(indextoDelete, 1);
