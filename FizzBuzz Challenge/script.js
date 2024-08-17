47c0369f0b7525372955d2f14bbeb5245073a7fa
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-17 20:11:35 +0800

    Update script.js

--- FizzBuzz Challenge/script.js
+++ FizzBuzz Challenge/script.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <= 100; i ++ ) {
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz')
+    if (i % 3 == 0 && i % 5 == 0) { // Checks if i is divisible by 3 and 5
+        console.log('FizzBuzz') 
     } else if (i % 3 == 0) {
         console.log('Fizz')
     } else if (i % 5 == 0) {
