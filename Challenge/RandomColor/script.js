c6f123734267c442d7b577b6e62bfd32909d6d69
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-17 19:30:17 +0800

    Add files via upload

--- Challenge/RandomColor/script.js
+++ Challenge/RandomColor/script.js
@@ -1,13 +1,13 @@
 const h1 = document.querySelector('h1');
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
-    const newColor = makeRandomColor();
+    const newColor = makeRandColor();
     document.body.style.backgroundColor = newColor;
     h1.innerHTML = newColor;
 })
 
 
-const makeRandomColor = () => {
+const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255)
     const g = Math.floor(Math.random() * 255)
     const b = Math.floor(Math.random() * 255)
