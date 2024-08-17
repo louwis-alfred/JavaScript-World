9bfc4ae87c5c591483ecccd297faa40b773827bd
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-17 20:13:01 +0800

    Update script.js

--- FizzBuzz Challenge/script.js
+++ FizzBuzz Challenge/script.js
@@ -9,3 +9,20 @@ for (let i = 1; i <= 100; i ++ ) {
 }
 
 // Output:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// 16
+// ...
