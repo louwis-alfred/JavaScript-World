0731daedb4334d276f1a410cf013b58b42c6d2ba
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-17 19:47:09 +0800

    Add files via upload

---
+++ Challenge/ButtonChangeColors/script.js
@@ -0,0 +1,30 @@
+
+const buttons = document.querySelectorAll('button');
+
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    
+    const newColor = `rgb(${r}, ${g}, ${b})`;
+
+    return newColor;
+}
+
+for (let button of buttons) {
+    button.addEventListener('click', colorized)
+}
+
+
+const h1s = document.querySelectorAll('h1');
+
+for (let h1 of h1s) {
+    h1.addEventListener('click', colorized);
+}
+
+// Using keyword "this"
+
+function colorized() {
+    this.style.backgroundColor = makeRandomColor();
+    this.style.color = makeRandomColor();
+}
