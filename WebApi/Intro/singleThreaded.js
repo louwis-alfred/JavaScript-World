516b8b573a6dfbe1086616e9617779f6630310ca
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-26 13:16:19 +0800

    Add files via upload

---
+++ WebApi/Intro/singleThreaded.js
@@ -0,0 +1,5 @@
+console.log('Hello World')
+setTimeout(() => {
+    console.log('Browser did a count for me, Sorry for the delay') // It transfer this call back to web api
+}, 3000);
+console.log('End line of code')
